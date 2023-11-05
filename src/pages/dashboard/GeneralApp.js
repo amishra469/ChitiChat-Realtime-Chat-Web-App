bdb02e1a847779e7b4236eb712b961fb1c470956
amishra469 <svmaditya01@gmail.com>
2023-11-05 14:54:30 +0530

    Added Chat Header

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Chats from "./Chats";
+import { Box, Stack } from "@mui/material";
+import Conversation from "../../components/Conversation";
 
 const GeneralApp = () => {
 
   return (
     <>
-      <Chats />
+      <Stack direction='row' sx={{width: "100%"}}>
+        <Chats />
+        <Box sx={{height: '100%', width: "calc(100vw - 420px)", backgroundColor: '#fff'}}>
+          <Conversation />
+        </Box>
+      </Stack>
     </>
   );
 };
